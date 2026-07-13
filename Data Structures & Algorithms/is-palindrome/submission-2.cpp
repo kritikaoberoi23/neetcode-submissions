8c1de078ffe60624f928824ad9cbc80f1abd528c
Kritika Oberoi <kritikaoberoi1023@gmail.com>
2026-07-13 18:30:52 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.length();
+        int i=0,j=n-1;
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        while(i<j){
+            while(i<j && !isalnum(s[i])) i++;
+            while(i<j && !isalnum(s[j])) j--;
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
